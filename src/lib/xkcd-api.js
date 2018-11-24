5f508b6cce82bebf8f187ce371d7e5b96a60f593
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2018-11-24 03:51:26 +0000

    Update xkcd-api.js

--- src/lib/xkcd-api.js
+++ src/lib/xkcd-api.js
@@ -1,5 +1,6 @@
 import jsonp from 'jsonp-promise';
 
+// xkcd's REST API does not have CORS enabled - however there is a JSONP API that can be used.
 const getLatestComic = async (number = undefined) => await jsonp(`http://dynamic.xkcd.com/api-0/jsonp/comic/${number}`).promise;
 
 export {
